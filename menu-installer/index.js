7fea29f8bb0a48b628a46ad188cb60240686f268
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-05-19 20:47:08 +0200

    fix: menu for db

--- menu-installer/index.js
+++ menu-installer/index.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer";
-import fs from 'fs';
-import mysql from 'mysql2/promise';
+import fs from "fs";
+import mysql from "mysql2/promise";
 function init_shell() {
     console.clear();
     console.log("*******************************", "*******************************", "*******************************\n\n");
@@ -37,49 +37,51 @@ async function show_menu() {
         {
             type: "password",
             name: "password",
-            message: "Write his password : "
+            message: "Write his password : ",
         },
         {
             type: "input",
             name: "userdbmysql",
-            message: "Write the username mysql (root recommanded) : "
+            message: "Write the username mysql (root recommanded) : ",
         },
         {
             type: "password",
             name: "passworddbmysql",
-            message: "Write the password mysql (root recommanded) : "
+            message: "Write the password mysql (root recommanded) : ",
         },
     ]);
     write_config(question.name, question.password, question.userdbmysql, question.passwordrootmysql);
 }
 function write_config(userdb, passdb, userrootmysql, passwordrootmysql) {
     const data = {
-        name: userdb,
-        password: passdb
+        USER_DB: userdb,
+        USER_DB_PASSWORD: passdb,
     };
-    const jsonData = JSON.stringify(data, null, 2);
-    fs.writeFile('configs.json', jsonData, (err) => {
+    const envData = Object.entries(data)
+        .map(([key, value]) => `${key}=${value}`)
+        .join("\n");
+    fs.writeFile("../ui/.env", envData, (err) => {
         if (err) {
-            console.error('An error occurred while writing to the file : ', err);
+            console.error("An error occurred while writing to the .env file:", err);
         }
         else {
             create_user_db(userdb, passdb, userrootmysql, passwordrootmysql);
-            console.log('\nData successfully written to configs.json\n');
+            console.log("Data successfully written to .env file");
         }
     });
 }
 async function connectionDB(userrootmysql, passwordrootmysql) {
     try {
         const connection = await mysql.createConnection({
-            host: 'localhost',
+            host: "localhost",
             user: userrootmysql,
-            password: passwordrootmysql
+            password: passwordrootmysql,
         });
-        console.log('Connected to the MySQL server.\n');
+        console.log("Connected to the MySQL server.\n");
         return connection;
     }
     catch (err) {
-        console.error('Error connecting to the database:', err);
+        console.error("Error connecting to the database:", err);
         throw err;
     }
 }
@@ -88,16 +90,16 @@ async function create_user_db(username, passwd, userrootmysql, passwordrootmysql
     try {
         const query = `CREATE USER '${username}'@'localhost' IDENTIFIED BY '${passwd}'`;
         await connection.execute(query, [username, passwd]);
-        console.log('MySQL user created successfully.\n');
+        console.log("MySQL user created successfully.\n");
         const grantPrivilegesQuery = `GRANT ALL PRIVILEGES ON nemesisbdd.* TO '${username}'@'localhost'`;
         await connection.execute(grantPrivilegesQuery, [username]);
         console.log(`Granted all privileges on nemesisbdd to ${username}.`);
         const flushPrivilegesQuery = `FLUSH PRIVILEGES`;
         await connection.execute(flushPrivilegesQuery);
-        console.log('Privileges flushed.');
+        console.log("Privileges flushed.");
     }
     catch (err) {
-        console.error('An error occurred while creating the MySQL user:', err);
+        console.error("An error occurred while creating the MySQL user:", err);
     }
     finally {
         await connection.end();
--- menu-installer/index.ts
+++ menu-installer/index.ts
@@ -1,6 +1,8 @@
 import inquirer from "inquirer";
-import fs from 'fs'
-import mysql from 'mysql2/promise';
+import fs from "fs";
+import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
+
 
 function init_shell() {
   console.clear();
@@ -47,7 +49,7 @@ function init_shell() {
 }
 
 async function show_menu() {
-    console.log("Creation user for database :\n")
+  console.log("Creation user for database :\n");
   const question = await inquirer.prompt([
     {
       type: "input",
@@ -55,75 +57,91 @@ async function show_menu() {
       message: "What is the name of user mysql do you want ?",
     },
     {
-        type: "password",
-        name: "password",
-        message: "Write his password : "
-
+      type: "password",
+      name: "password",
+      message: "Write his password : ",
     },
     {
-        type: "input",
-        name: "userdbmysql",
-        message: "Write the username mysql (root recommanded) : "
+      type: "input",
+      name: "userdbmysql",
+      message: "Write the username mysql (root recommanded) : ",
     },
     {
-        type: "password",
-        name: "passworddbmysql",
-        message: "Write the password mysql (root recommanded) : "
+      type: "password",
+      name: "passworddbmysql",
+      message: "Write the password mysql (root recommanded) : ",
     },
-    
   ]);
 
-  write_config(question.name, question.password, question.userdbmysql, question.passwordrootmysql)
-  
+  write_config(
+    question.name,
+    question.password,
+    question.userdbmysql,
+    question.passwordrootmysql
+  );
 }
-function write_config(userdb: string, passdb: string, userrootmysql: string, passwordrootmysql: string) {
-    const data = {
-        name: userdb,
-        password: passdb
+function write_config(
+  userdb: string,
+  passdb: string,
+  userrootmysql: string,
+  passwordrootmysql: string
+) {
+  const data = {
+    USER_DB: userdb,
+    USER_DB_PASSWORD: passdb,
+  };
+
+  const envData = Object.entries(data)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("\n");
+
+  fs.writeFile("../ui/.env", envData, (err) => {
+    if (err) {
+      console.error("An error occurred while writing to the .env file:", err);
+    } else {
+      create_user_db(userdb, passdb, userrootmysql, passwordrootmysql);
+      console.log("Data successfully written to .env file");
     }
-    const jsonData = JSON.stringify(data, null, 2)
-    fs.writeFile('configs.json', jsonData, (err) => {
-        if(err) {
-            console.error('An error occurred while writing to the file : ', err)
-        } else {
-            create_user_db(userdb, passdb, userrootmysql, passwordrootmysql)
-            console.log('\nData successfully written to configs.json\n')
-        }
-    })
+  });
 }
 
 async function connectionDB(userrootmysql: string, passwordrootmysql: string) {
-    try {
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: userrootmysql, 
-            password: passwordrootmysql 
-        });
+  try {
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: userrootmysql,
+      password: passwordrootmysql,
+    });
 
-        console.log('Connected to the MySQL server.\n');
-        return connection;
-    } catch (err) {
-        console.error('Error connecting to the database:', err);
-        throw err;
-    }
+    console.log("Connected to the MySQL server.\n");
+    return connection;
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+    throw err;
+  }
 }
-async function create_user_db(username: string, passwd: string, userrootmysql: string, passwordrootmysql: string) {
-    const connection = await connectionDB(userrootmysql, passwordrootmysql);
-    try {
-        const query = `CREATE USER '${username}'@'localhost' IDENTIFIED BY '${passwd}'`;
-        await connection.execute(query, [username, passwd]);
-        console.log('MySQL user created successfully.\n');
-        const grantPrivilegesQuery = `GRANT ALL PRIVILEGES ON nemesisbdd.* TO '${username}'@'localhost'`;
-        await connection.execute(grantPrivilegesQuery, [username]);
-        console.log(`Granted all privileges on nemesisbdd to ${username}.`);
-        const flushPrivilegesQuery = `FLUSH PRIVILEGES`;
-        await connection.execute(flushPrivilegesQuery);
-        console.log('Privileges flushed.');
-    } catch (err) {
-        console.error('An error occurred while creating the MySQL user:', err);
-    } finally {
-        await connection.end();
-    }
+async function create_user_db(
+  username: string,
+  passwd: string,
+  userrootmysql: string,
+  passwordrootmysql: string
+) {
+  const connection = await connectionDB(userrootmysql, passwordrootmysql);
+  try {
+    const query = `CREATE USER '${username}'@'localhost' IDENTIFIED BY '${passwd}'`;
+    await connection.execute(query, [username, passwd]);
+    console.log("MySQL user created successfully.\n");
+    const grantPrivilegesQuery = `GRANT ALL PRIVILEGES ON nemesisbdd.* TO '${username}'@'localhost'`;
+    await connection.execute(grantPrivilegesQuery, [username]);
+    console.log(`Granted all privileges on nemesisbdd to ${username}.`);
+    const flushPrivilegesQuery = `FLUSH PRIVILEGES`;
+    await connection.execute(flushPrivilegesQuery);
+    console.log("Privileges flushed.");
+  } catch (err) {
+    console.error("An error occurred while creating the MySQL user:", err);
+  } finally {
+    await connection.end();
+  }
 }
 
 function princip_menu() {
