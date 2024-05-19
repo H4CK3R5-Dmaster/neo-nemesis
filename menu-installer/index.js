9a879a1a922997f91982f1b0257d5149845cd7b5
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-05-19 17:14:13 +0200

    feat: installer

--- menu-installer/index.js
+++ menu-installer/index.js
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+import fs from 'fs';
 function init_shell() {
     console.clear();
     console.log("*******************************", "*******************************", "*******************************\n\n");
@@ -11,35 +12,49 @@ function init_shell() {
     console.log("\t\t\t\t⠀⠙⠿⢾⣤⡈⠙⠂⢤⢀⠀⠙⠿⢿⣿⣿⡿⠟⠁⠀⣀⣀⣤⣶⠟⠋⠁⠀⠀⠀");
     console.log("\t\t\t\t⠀⠀⠀⠀⠈⠙⠿⣾⣠⣆⣅⣀⣠⣄⣤⣴⣶⣾⣽⢿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀");
     console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠙⠋⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
-    console.log('\n');
+    console.log("\n");
     console.log("\t███    ██ ███████ ███████         ███    ██ ███████ ███    ███ ███████ ███████ ██ ███████ ");
     console.log("\t████   ██ ██      ██   ██         ████   ██ ██      ████  ████ ██      ██      ██ ██      ");
     console.log("\t██ ██  ██ █████   ██   ██ ███████ ██ ██  ██ █████   ██ ████ ██ █████   ███████ ██ ███████ ");
     console.log("\t██  ██ ██ ██      ██   ██         ██  ██ ██ ██      ██  ██  ██ ██           ██ ██      ██ ");
     console.log("\t██   ████ ███████ ███████         ██   ████ ███████ ██      ██ ███████ ███████ ██ ███████ ");
-    console.log('\n');
+    console.log("\n");
     console.log("\t\t\t\t-HOW ABOUT CHECKING FILE PROPERTIES ?-");
     console.log("\t\t\t\t-v1.2 Nemesis Group-");
-    console.log('\n');
+    console.log("\n");
     console.log("*******************************", "*******************************", "*******************************\n\n");
     console.log("\t\t\t\t - Monitoring installer -\n\n");
 }
 async function show_menu() {
+    console.log("Creation user for database :\n");
     const question = await inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "Hello who am i ?"
+            message: "What is the name of user mysql do you want ?",
         },
         {
-            type: "list",
-            name: "listchoice",
-            message: "Select",
-            choices: ["mysql", "postgresql", "mongodb"]
+            type: "password",
+            name: "password",
+            message: "Write his password : "
         }
     ]);
-    console.log(question.name);
-    console.log(question.listchoice);
+    write_config(question.name, question.password);
+}
+function write_config(userdb, passdb) {
+    const data = {
+        name: userdb,
+        password: passdb
+    };
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFile('configs.json', jsonData, (err) => {
+        if (err) {
+            console.error('An error occurred while writing to the file : ', err);
+        }
+        else {
+            console.log('Data successfully written to configs.json');
+        }
+    });
 }
 function princip_menu() {
     init_shell();
--- menu-installer/index.ts
+++ menu-installer/index.ts
@@ -1,57 +1,88 @@
 import inquirer from "inquirer";
+import fs from 'fs'
 
 function init_shell() {
-    console.clear()
-    console.log("*******************************",
-        "*******************************",
-        "*******************************\n\n");
-    console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⠀  ⢀⣀⣤⣤⣤⣤⣴⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
-    console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⠿⠛⠋⠉⠁⠀⠀⠀⠈⠙⠻⢷⣦⡀⠀⠀⠀⠀⠀⠀");
-    console.log("\t\t\t\t⠀⠀⠀⠀⠀⣤⣾⡿⠋⠁⠀⣠⣶⣿⡿⢿⣷⣦⡀⠀⠀⠀⠙⠿⣦⣀⠀⠀⠀⠀");
-    console.log("\t\t\t\t⠀⠀⢀⣴⣿⡿⠋⠀⠀⢀⣼⣿⣿⣿⣶⣿⣾⣽⣿⡆⠀⠀⠀⠀⢻⣿⣷⣶⣄⠀");
-    console.log("\t\t\t\t⠀⣴⣿⣿⠋⠀⠀⠀⠀⠸⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⠀⠀⠀⠐⡄⡌⢻⣿⣿⡷");
-    console.log("\t\t\t\t⢸⣿⣿⠃⢂⡋⠄⠀⠀⠀⢿⣿⣿⣿⣿⣿⣯⣿⣿⠏⠀⠀⠀⠀⢦⣷⣿⠿⠛⠁");
-    console.log("\t\t\t\t⠀⠙⠿⢾⣤⡈⠙⠂⢤⢀⠀⠙⠿⢿⣿⣿⡿⠟⠁⠀⣀⣀⣤⣶⠟⠋⠁⠀⠀⠀");
-    console.log("\t\t\t\t⠀⠀⠀⠀⠈⠙⠿⣾⣠⣆⣅⣀⣠⣄⣤⣴⣶⣾⣽⢿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀");
-    console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠙⠋⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
-    console.log('\n')
-    console.log("\t███    ██ ███████ ███████         ███    ██ ███████ ███    ███ ███████ ███████ ██ ███████ ");
-    console.log("\t████   ██ ██      ██   ██         ████   ██ ██      ████  ████ ██      ██      ██ ██      ");
-    console.log("\t██ ██  ██ █████   ██   ██ ███████ ██ ██  ██ █████   ██ ████ ██ █████   ███████ ██ ███████ ");
-    console.log("\t██  ██ ██ ██      ██   ██         ██  ██ ██ ██      ██  ██  ██ ██           ██ ██      ██ ");
-    console.log("\t██   ████ ███████ ███████         ██   ████ ███████ ██      ██ ███████ ███████ ██ ███████ ");
-    console.log('\n')
-    console.log("\t\t\t\t-HOW ABOUT CHECKING FILE PROPERTIES ?-");
-    console.log("\t\t\t\t-v1.2 Nemesis Group-");
-    console.log('\n')
-    console.log("*******************************",
-        "*******************************",
-        "*******************************\n\n");
-    console.log("\t\t\t\t - Monitoring installer -\n\n");
+  console.clear();
+  console.log(
+    "*******************************",
+    "*******************************",
+    "*******************************\n\n"
+  );
+  console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⠀  ⢀⣀⣤⣤⣤⣤⣴⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+  console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⠿⠛⠋⠉⠁⠀⠀⠀⠈⠙⠻⢷⣦⡀⠀⠀⠀⠀⠀⠀");
+  console.log("\t\t\t\t⠀⠀⠀⠀⠀⣤⣾⡿⠋⠁⠀⣠⣶⣿⡿⢿⣷⣦⡀⠀⠀⠀⠙⠿⣦⣀⠀⠀⠀⠀");
+  console.log("\t\t\t\t⠀⠀⢀⣴⣿⡿⠋⠀⠀⢀⣼⣿⣿⣿⣶⣿⣾⣽⣿⡆⠀⠀⠀⠀⢻⣿⣷⣶⣄⠀");
+  console.log("\t\t\t\t⠀⣴⣿⣿⠋⠀⠀⠀⠀⠸⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⠀⠀⠀⠐⡄⡌⢻⣿⣿⡷");
+  console.log("\t\t\t\t⢸⣿⣿⠃⢂⡋⠄⠀⠀⠀⢿⣿⣿⣿⣿⣿⣯⣿⣿⠏⠀⠀⠀⠀⢦⣷⣿⠿⠛⠁");
+  console.log("\t\t\t\t⠀⠙⠿⢾⣤⡈⠙⠂⢤⢀⠀⠙⠿⢿⣿⣿⡿⠟⠁⠀⣀⣀⣤⣶⠟⠋⠁⠀⠀⠀");
+  console.log("\t\t\t\t⠀⠀⠀⠀⠈⠙⠿⣾⣠⣆⣅⣀⣠⣄⣤⣴⣶⣾⣽⢿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀");
+  console.log("\t\t\t\t⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠙⠋⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+  console.log("\n");
+  console.log(
+    "\t███    ██ ███████ ███████         ███    ██ ███████ ███    ███ ███████ ███████ ██ ███████ "
+  );
+  console.log(
+    "\t████   ██ ██      ██   ██         ████   ██ ██      ████  ████ ██      ██      ██ ██      "
+  );
+  console.log(
+    "\t██ ██  ██ █████   ██   ██ ███████ ██ ██  ██ █████   ██ ████ ██ █████   ███████ ██ ███████ "
+  );
+  console.log(
+    "\t██  ██ ██ ██      ██   ██         ██  ██ ██ ██      ██  ██  ██ ██           ██ ██      ██ "
+  );
+  console.log(
+    "\t██   ████ ███████ ███████         ██   ████ ███████ ██      ██ ███████ ███████ ██ ███████ "
+  );
+  console.log("\n");
+  console.log("\t\t\t\t-HOW ABOUT CHECKING FILE PROPERTIES ?-");
+  console.log("\t\t\t\t-v1.2 Nemesis Group-");
+  console.log("\n");
+  console.log(
+    "*******************************",
+    "*******************************",
+    "*******************************\n\n"
+  );
+  console.log("\t\t\t\t - Monitoring installer -\n\n");
 }
 
 async function show_menu() {
-    const question = await inquirer.prompt([
-        {
-        type: "input",
-        name: "name",
-        message: "Hello who am i ?"
-    }, 
+    console.log("Creation user for database :\n")
+  const question = await inquirer.prompt([
     {
-        type: "list",
-        name: "listchoice",
-        message: "Select",
-        choices: ["mysql", "postgresql", "mongodb"]
-    }
-]);
+      type: "input",
+      name: "name",
+      message: "What is the name of user mysql do you want ?",
+    },
+    {
+        type: "password",
+        name: "password",
+        message: "Write his password : "
 
-    console.log(question.name)
-    console.log(question.listchoice)
+    }
+    
+  ]);
+
+  write_config(question.name, question.password)
+  
+}
+function write_config(userdb: String, passdb: String) {
+    const data = {
+        name: userdb,
+        password: passdb
+    }
+    const jsonData = JSON.stringify(data, null, 2)
+    fs.writeFile('configs.json', jsonData, (err) => {
+        if(err) {
+            console.error('An error occurred while writing to the file : ', err)
+        } else {
+            console.log('Data successfully written to configs.json')
+        }
+    })
 }
 
 function princip_menu() {
-    init_shell()
-    show_menu()
+  init_shell();
+  show_menu();
 }
 
-princip_menu()
+princip_menu();
